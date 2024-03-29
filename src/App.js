769117ac9a2436d8f205802f37b763c6e97521dc
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-03-29 21:54:53 +0530

    updated flag

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Button, Container, Form } from 'react-bootstrap';
+import { Button, Container, Form, Row, Col, Spinner } from 'react-bootstrap';
+import axios from 'axios';
+import PieChart from './components/PieChart';
+import Info from './components/Info';
+import Gauge from './components/Gauge.js';
+import MapTile from './components/maptile.png';
+import MarkerMap from './components/MarkerMap.js';
+
 
 function App() {
   const [countryName, setCountryName] = useState('');
+  const [showComponents, setShowComponents] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [lastSavedCountry, setLastSavedCountry] = useState(null);
+  const [pieChartData, setPieChartData] = useState(null);
 
-  const handleSubmit = (event) => {
+  useEffect(() => {
+    // Fetch the last saved country information when the component mounts
+    getLastSavedCountry();
+  }, []);
+
+  const getLastSavedCountry = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('http://localhost:30001/getLastSavedCountryInfo');
+      console.log('Last saved country:', response.data);
+      setLastSavedCountry(response.data);
+      const labels = response.data.languages.map(lang => lang);
+      const data = response.data.languages.map(() => response.data.languages.length); // Dummy data, change according to your requirements
+      setPieChartData({
+        labels: labels,
+        datasets: [{
+          label: 'Languages Spoken',
+          data: data,
+        }]
+      });
+
+      setLoading(false);
+    }
+    catch (error) {
+      setLoading(false);
+      console.error('Error fetching last saved country information:', error);
+    }
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("Country Name submitted:", countryName);
+    try {
+      setLoading(true);
+      const response = await axios.get(`http://localhost:30001/fetchAndSaveCountryInfo?countryName=${countryName}`);
+      console.log(response.data.message);
+      setShowComponents(true);
+      getLastSavedCountry(); // Fetch the last saved country information after saving a new country
+    } catch (error) {
+      setLoading(false);
+      console.error('Error submitting country name:', error);
+    }
   };
 
   const handleChange = (event) => {
@@ -16,22 +65,51 @@ function App() {
 
   return (
     <div>
-      <Container>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="countryName">
-            <Form.Control
-              type="text"
-              value={countryName}
-              onChange={handleChange}
-              placeholder="Enter country name"
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+      <Container className='mt-5 d-flex justify-content-center align-items-center'>
+        <Row className='d-flex justify-content-center align-items-center'>
+
+          <Form onSubmit={handleSubmit} className="d-flex align-items-center">
+            <Form.Group controlId="countryName" className="mr-2 mb-0">
+              <Form.Control
+                type="text"
+                value={countryName}
+                onChange={handleChange}
+                placeholder="Enter country name"
+              />
+            </Form.Group>
+            <Button className='mx-3' variant="primary" type="submit">
+              {loading ? <Spinner animation="border" variant="light" size="sm" /> : 'Search'}
+            </Button>
+          </Form>
+        </Row>
       </Container>
-    </div>
+      {
+        showComponents && lastSavedCountry && (
+          <div>
+            <Container>
+              <Row className='my-3 justify-content-center align-items-center'>
+
+                <Col lg={5} md={12} sm={12} className='d-flex px-5 mx-4 justify-content-between align-items-center box'>
+
+                  <Info {...lastSavedCountry} />
+                </Col>
+                <Col lg={5} md={12} sm={12} className='d-flex justify-content-center align-items-center box'>
+                  <Gauge level={lastSavedCountry.populationCategory} />
+                </Col>
+              </Row>
+              <Row className='my-3 justify-content-center align-items-center' >
+                <Col lg={5} md={12} sm={12} className='d-flex mx-4 justify-content-center align-items-center box'>
+                  {pieChartData && <PieChart data={pieChartData} />}
+                </Col>
+                <Col lg={5} md={12} sm={12} className='d-flex justify-content-center align-items-center box'>
+                  <MarkerMap latitude={lastSavedCountry.latitude} longitude={lastSavedCountry.longitude} />
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        )
+      }
+    </div >
   );
 }
 
