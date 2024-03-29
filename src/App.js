9897f8b199c78e60e1e858683cd944f5510961a0
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-03-29 12:37:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, Container, Form } from 'react-bootstrap';
 
 function App() {
+  const [countryName, setCountryName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Country Name submitted:", countryName);
+  };
+
+  const handleChange = (event) => {
+    setCountryName(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="countryName">
+            <Form.Control
+              type="text"
+              value={countryName}
+              onChange={handleChange}
+              placeholder="Enter country name"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }
